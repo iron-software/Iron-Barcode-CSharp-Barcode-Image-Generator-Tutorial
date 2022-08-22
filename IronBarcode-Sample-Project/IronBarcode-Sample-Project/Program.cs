4bcb2a8db821466dc928e16ef18f19074a00c840
Michael B <themikebee2000@gmail.com>
2022-08-22 16:03:26 +0700

    Blank Project

---
+++ IronBarcode-Sample-Project/IronBarcode-Sample-Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IronBarcode_Sample_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
